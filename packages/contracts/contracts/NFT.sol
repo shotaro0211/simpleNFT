7f4231f368d2f70391a1e1259506a277cde3f296
shotaro0211 <shotaro.ibramessi@gmail.com>
2021-08-30 19:20:29 +0900

    [Improve]

--- packages/contracts/contracts/NFT.sol
+++ packages/contracts/contracts/NFT.sol
@@ -3,5 +3,9 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 contract NFT is ERC721PresetMinterPauserAutoId {
-  constructor() ERC721PresetMinterPauserAutoId("name", "symbol", "http://localhost:3000") {}
+  constructor() ERC721PresetMinterPauserAutoId("Clione", "CLN", "https://8mart-staging.s3.ap-northeast-1.amazonaws.com/nft/clione/") {}
+
+  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+     return string(abi.encodePacked(super.tokenURI(tokenId),".json"));
+  }
 }
--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Header from './Header'
 //ethereumのライブラリー
 import { ethers } from "ethers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 //NFT.solのdeployファイルをとりこむ。cracoを使ってModuleScopePluginの設定を削除しないとimportできない。
 import {abi,bytecode} from "../../contracts/artifacts/contracts/NFT.sol/NFT.json";
 
 //Providerを作成。Ethereumへのネットワーク接続を管理します。
 const provider = new ethers.providers.Web3Provider(window.ethereum);
-let address = "" //NFTのdeploy時のアドレスを指定する。
+let address = "0x604d6ea0f769e0d48349107a961dae47c8466d43" //NFTのdeploy時のアドレスを指定する。
 //Clickの処理.NFTをDeploy
 const buttonDeploy = async() => {
   //署名を取得します。コントラクトの作成には署名が必要です。基本的に、コントラクトの生成や値が変わる処理にはマイニングが必要なので署名が必要です。
@@ -47,14 +51,59 @@ const buttonSupply = async() => {
   console.log(address);
   console.log(await contract.totalSupply());
 };
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root');
 function App() {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <div className="App">
-    <p>動作</p>
-    <button id="test" onClick={buttonDeploy}>NFT deploy</button><br/>
-    <button id="test1" onClick={buttonGetName}>NFT get name</button><br/>
-    <button id="test2" onClick={buttonMint}>NFT mint</button><br/>
-    <button id="test2" onClick={buttonSupply}>NFT totalSupply</button>
+      <Header />
+      <div className="Top">
+        <div className="Title">クリオネ募金<span className="TitleMoney">寄付総額: 7123197円</span></div>
+        <div className="DescriptionContainer">
+          <div className="Description">
+            あなたの寄付が報われる世界へ<br /><br />
+            あなたの思いを本当に困っている人へ送り届けます
+          </div>
+        </div>
+        <div className="ButtonContainer">
+          <button className="Button" onClick={() => setIsOpen(true)}>寄付する</button>
+        </div>
+        <Modal className="Modal" isOpen={modalIsOpen}>
+          <div className="Select">
+            <div>福島県沖の地震<span className="SelectContainer"><button className="SelectButton" id="test" onClick={buttonDeploy}>寄付する</button></span><br/><br/></div>
+            <div>松江市火災<span className="SelectContainer"><button className="SelectButton" id="test" onClick={buttonDeploy}>寄付する</button></span><br/><br/></div>
+            <div>山形県沖の地震<span className="SelectContainer"><button className="SelectButton" id="test" onClick={buttonDeploy}>寄付する</button></span><br/><br/></div>
+            <div>島根県吉賀町火災<span className="SelectContainer"><button className="SelectButton" id="test" onClick={buttonDeploy}>寄付する</button></span><br/><br/></div>
+            <div>島根県吉賀町火災<span className="SelectContainer"><button className="SelectButton" id="test2" onClick={buttonMint}>寄付する</button></span><br/><br/></div>
+          </div>
+        </Modal>
+      </div>
     </div>
   );
 }
