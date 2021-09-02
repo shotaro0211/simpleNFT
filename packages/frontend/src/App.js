b3574d088bd4e8f5435846dbbaf0afc620c4fd1b
shotaro0211 <shotaro.ibramessi@gmail.com>
2021-09-02 18:27:34 +0900

    [Fixed]Tanzawa

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,55 +1,53 @@
 import './App.css';
-import Header from './Header'
-//ethereumのライブラリー
 import { ethers } from "ethers";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Modal from 'react-modal';
-//NFT.solのdeployファイルをとりこむ。cracoを使ってModuleScopePluginの設定を削除しないとimportできない。
 import {abi,bytecode} from "../../contracts/artifacts/contracts/NFT.sol/NFT.json";
 
-//Providerを作成。Ethereumへのネットワーク接続を管理します。
+window.ethereum.enable();
 const provider = new ethers.providers.Web3Provider(window.ethereum);
-let address = "0x604d6ea0f769e0d48349107a961dae47c8466d43" //NFTのdeploy時のアドレスを指定する。
-//Clickの処理.NFTをDeploy
+let address = "0x604d6ea0f769e0d48349107a961dae47c8466d43" 
+let total_supply = 0
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+const pass = getParam('password');
+let password = ['リンゴ', 'バナナ', 'パイナップル', 'グレープ'];
 const buttonDeploy = async() => {
-  //署名を取得します。コントラクトの作成には署名が必要です。基本的に、コントラクトの生成や値が変わる処理にはマイニングが必要なので署名が必要です。
   const signer = provider.getSigner();
-  //abi,bytecode,署名からコントラクトを作成するためのfactoryを作ります。
   const factory = new ethers.ContractFactory(abi,bytecode,signer);
-  //NFTをdeployします。
-  const contract = await factory.deploy();//metamaskの署名を要求する
-  address = contract.address;//deployしたアドレス。
-  console.log(contract);
-  console.log(address);
-  //rinkebyのchainidのときはetherscanのリンクを表示
+  const contract = await factory.deploy();
+  address = contract.address;
   const net = await signer.provider.getNetwork();
-  if( net.chainId === 4) console.log("https://rinkeby.etherscan.io/tx/" + contract.deployTransaction.hash);//Tx Hash Etherscanで探す。
+  if( net.chainId === 4) console.log("https://rinkeby.etherscan.io/tx/" + contract.deployTransaction.hash);
 };
-//Clickの処理.consoleにnameが表示されればOK.Txがcommitされる前に押すとエラーになる。
 const buttonGetName = async() => {
   const contract = new ethers.Contract(address, abi, provider);
   console.log(await contract.name())
   console.log(address);
 };
-//Clickの処理.NFTをmint
 const buttonMint = async() => {
-  const signer = provider.getSigner();
-  //以前にdeployしたコントラクトのアドレスからコントラクトを特定します。
-  const contract = new ethers.Contract(address, abi, provider);
-  //署名を付けてコントラクトのmintを実行します。
-  const {hash } = await contract.connect(signer).mint(signer.getAddress());//metamaskの署名を要求する
-  console.log(contract);
-  console.log(address);
-  //rinkebyのchainidのときはetherscanのリンクを表示
-  const net = await signer.provider.getNetwork();
-  if( net.chainId === 4) console.log("https://rinkeby.etherscan.io/tx/" + hash);//Tx Hash Etherscanで探す。
-};
-//Clickの処理.NFTのtotalSupply
-const buttonSupply = async() => {
   const contract = new ethers.Contract(address, abi, provider);
   console.log(address);
-  console.log(await contract.totalSupply());
+  const supply = await contract.totalSupply()
+  total_supply = supply._hex.slice(3);
+  console.log(total_supply);
+  if (total_supply <= 7) { 
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(address, abi, provider);
+    const {hash } = await contract.connect(signer).mint(signer.getAddress());
+    console.log(contract);
+    console.log(address);
+    const net = await signer.provider.getNetwork();
+    if( net.chainId === 4) console.log("https://rinkeby.etherscan.io/tx/" + hash);
+  } else {
+  };
 };
 const customStyles = {
   content: {
@@ -62,49 +60,18 @@ const customStyles = {
   },
 };
 
-// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
-Modal.setAppElement('#root');
 function App() {
-  let subtitle;
-  const [modalIsOpen, setIsOpen] = React.useState(false);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
   return (
     <div className="App">
-      <Header />
       <div className="Top">
-        <div className="Title">クリオネ募金<span className="TitleMoney">寄付総額: 7123197円</span></div>
-        <div className="DescriptionContainer">
-          <div className="Description">
-            あなたの寄付が報われる世界へ<br /><br />
-            あなたの思いを本当に困っている人へ送り届けます
-          </div>
-        </div>
+        <div className="Title">丹沢山頂到着!!!</div>
+        <div className="Description">山頂の景色を画像で送信し、NFTを獲得せよ</div>
         <div className="ButtonContainer">
-          <button className="Button" onClick={() => setIsOpen(true)}>寄付する</button>
+          <button className="Button" id="test2" onClick={buttonMint}>獲得する</button>
         </div>
-        <Modal className="Modal" isOpen={modalIsOpen}>
-          <div className="Select">
-            <div>福島県沖の地震<span className="SelectContainer"><button className="SelectButton" id="test" onClick={buttonDeploy}>寄付する</button></span><br/><br/></div>
-            <div>松江市火災<span className="SelectContainer"><button className="SelectButton" id="test" onClick={buttonDeploy}>寄付する</button></span><br/><br/></div>
-            <div>山形県沖の地震<span className="SelectContainer"><button className="SelectButton" id="test" onClick={buttonDeploy}>寄付する</button></span><br/><br/></div>
-            <div>島根県吉賀町火災<span className="SelectContainer"><button className="SelectButton" id="test" onClick={buttonDeploy}>寄付する</button></span><br/><br/></div>
-            <div>島根県吉賀町火災<span className="SelectContainer"><button className="SelectButton" id="test2" onClick={buttonMint}>寄付する</button></span><br/><br/></div>
-          </div>
-        </Modal>
       </div>
     </div>
   );
 }
+
 export default App;
