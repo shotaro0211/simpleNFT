d2657b3adff44312bafbd31d0adfec1e7a17e280
Shotaro Yamazaki <54551711+shotaro0211@users.noreply.github.com>
2021-09-22 09:51:43 +0900

    Update NFT.sol

--- packages/contracts/contracts/NFT.sol
+++ packages/contracts/contracts/NFT.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 contract NFT is ERC721PresetMinterPauserAutoId {
-  constructor() ERC721PresetMinterPauserAutoId("Shibusawa", "SBW", "https://8mart-staging.s3.ap-northeast-1.amazonaws.com/nft/shibusawa/") {}
+  constructor() ERC721PresetMinterPauserAutoId("Shibusawa", "SBW", "http://localhost:3000/") {}
 
   function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
      return string(abi.encodePacked(super.tokenURI(tokenId),".json"));
