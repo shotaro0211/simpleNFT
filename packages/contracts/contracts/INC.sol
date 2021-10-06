590068aab6af94e21ff871d05c52a612669d252b
Shotaro Yamazaki <54551711+shotaro0211@users.noreply.github.com>
2021-10-06 23:36:24 +0900

    Update INC.sol

--- packages/contracts/contracts/INC.sol
+++ packages/contracts/contracts/INC.sol
@@ -1,6 +1,5 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 contract INC is ERC721PresetMinterPauserAutoId {
   constructor() ERC721PresetMinterPauserAutoId("Shotaro", "SS", "https://dentou-s3.s3.ap-northeast-1.amazonaws.com/NFT/test/") {}
